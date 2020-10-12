e8cd912f4d88d2af8335ab61b26094e7d366b097
etaoxing <etaoxing@users.noreply.github.com>
2020-10-12 09:58:56 -0400

    change render (shadow needed for burner light)

--- adept_envs/adept_envs/franka/kitchen_multitask_v0.py
+++ adept_envs/adept_envs/franka/kitchen_multitask_v0.py
@@ -295,11 +295,10 @@ class KitchenTaskRelaxV1(KitchenV0):
             if camera_id is None: camera_id = self.camera_id
             camera = engine.MovableCamera(self.sim, height=height, width=width)
             camera.set_pose(**CAMERAS[camera_id])
-            
+
             # http://www.mujoco.org/book/APIreference.html#mjvOption
             # https://github.com/deepmind/dm_control/blob/9e0fe0f0f9713a2a993ca78776529011d6c5fbeb/dm_control/mujoco/engine.py#L200
             # mjtRndFlag(mjRND_SHADOW=0, mjRND_WIREFRAME=1, mjRND_REFLECTION=2, mjRND_ADDITIVE=3, mjRND_SKYBOX=4, mjRND_FOG=5, mjRND_HAZE=6, mjRND_SEGMENT=7, mjRND_IDCOLOR=8, mjNRNDFLAG=9)
-            # img = camera.render(render_flag_overrides=dict(shadow=False, reflection=False, skybox=False, fog=False, haze=False))
             img = camera.render(render_flag_overrides=dict(skybox=False, fog=False, haze=False))
             return img
         else:
